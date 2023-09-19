dba9621c33e10c0397c454ba11c712551eee2a5a
“Alba” <albaginesgarcia25@gmail.com>
2023-09-19 09:21:24 +0200

    created index html and main js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const select = document.querySelector('.js-select');
+const btnPlay = document.querySelector('.js-btn');
+const btnMessage = document.querySelector('.js-btnMessage');
+
+
